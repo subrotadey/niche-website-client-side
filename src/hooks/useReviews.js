cc7b99d4a7de90b47ea8780f3d2df04d8ab26f50
Subrota  Dey <78078777+subrotadey@users.noreply.github.com>
2023-02-01 02:47:47 +0600

    Update useReviews.js

--- src/hooks/useReviews.js
+++ src/hooks/useReviews.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 const useReviews = () => {
     const [reviews, setReviews] = useState([])
     useEffect(() => {
-        fetch('https://agile-garden-87094.herokuapp.com/reviews')
+        fetch('https://carniche.onrender.com/reviews')
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [reviews])
