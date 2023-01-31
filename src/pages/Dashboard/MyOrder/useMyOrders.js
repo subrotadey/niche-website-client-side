29e8b87c771fbee8d3f7bd2a2d26f3db50b0cbc0
Subrota  Dey <78078777+subrotadey@users.noreply.github.com>
2023-02-01 02:52:29 +0600

    Update useMyOrders.js

--- src/pages/Dashboard/MyOrder/useMyOrders.js
+++ src/pages/Dashboard/MyOrder/useMyOrders.js
@@ -5,7 +5,7 @@ const useMyOrders = () => {
     const { user } = useAuth()
     const [myOrders, setMyOrders] = useState([])
     useEffect(() => {
-        fetch(`https://agile-garden-87094.herokuapp.com/orders/${user?.email}`)
+        fetch(`https://carniche.onrender.com/orders/${user?.email}`)
             .then(res => res.json())
             .then(data => setMyOrders(data))
     }, [user, myOrders])
